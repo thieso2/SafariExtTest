b71a121457c337b023ab7c997a9a8c2639766a26
Thies C. Arntzen <thieso@gmail.com>
2021-04-12 15:16:24 +0200

    Initial Commit

---
+++ SafariExtTest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SafariExtTest
+//
+//  Created by Thies C. Arntzen on 12.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
